56fc800d5d2db3e20ac90395c2e37dd11b403a02
debdut066 <nilkarpas@gmail.com>
2021-12-21 12:48:37 +0530

    first commit

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -11,7 +11,7 @@ const LeftArrow = () => {
     <Flex justifyContent='center' alignItems='center' marginRight='1'>
       <Icon
         as={FaArrowAltCircleLeft}
-        onClick={() => scrollPrev()}
+        onClick={()=>scrollPrev()}
         fontSize='2xl'
         cursor='pointer'
         d={['none','none','none','block']}
@@ -39,7 +39,7 @@ export default function ImageSrollbar({ data }) {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
       {data.map((item) => (
-        <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
+        <Box width='910px' itemID={item.id} overflow='hidden' p='1'key={item.id}>
           <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
         </Box>
       ))}
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get((url), {
-    headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
-    },
-  });
-    
-  return data;
+    const { data } = await axios.get((url),{
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '158a178e06msh3a9b9c4dd7a0f50p1f7034jsna99e2061009a'
+        },
+    });
+
+    return data;
 }
